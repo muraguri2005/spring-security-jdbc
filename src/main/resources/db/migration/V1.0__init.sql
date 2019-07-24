476b55068f865e38832bb10bcfd9ecd85db196d6
Richard Gathogo <richard@tulaa.io>
2019-07-24 08:23:44 +0300

    update to flyawaydb to database migration

---
+++ src/main/resources/db/migration/V1.0__init.sql
@@ -0,0 +1,38 @@
+create table if not exists oauth_client_details (
+    client_id varchar(256) primary key,
+    resource_ids varchar(256),
+    client_secret varchar(256),
+    scope varchar(256),
+    authorized_grant_types varchar(256),
+    web_server_redirect_uri varchar(256),
+    authorities varchar(256),
+    access_token_validity integer,
+    refresh_token_validity integer,
+    additional_information varchar(4096),
+    autoapprove varchar(256)
+);
+
+create table if not exists oauth_access_token (
+  token_id VARCHAR(256),
+  token LONGVARBINARY,
+  authentication_id VARCHAR(256) PRIMARY KEY,
+  user_name VARCHAR(256),
+  client_id VARCHAR(256),
+  authentication LONGVARBINARY,
+  refresh_token VARCHAR(256)
+);
+
+create table if not exists oauth_refresh_token (
+  token_id VARCHAR(256),
+  token LONGVARBINARY,
+  authentication LONGVARBINARY
+);
+
+
+CREATE TABLE IF NOT EXISTS users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(256) NOT NULL,
+  password VARCHAR(256) NOT NULL,
+  enabled boolean not null default false,
+  UNIQUE KEY unique_username(username)
+);
