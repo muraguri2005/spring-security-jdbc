8ee2e15fb722c0445f43dc760a341100b40f02b5
Richard Gathogo <muraguri2005@gmail.com>
2019-08-08 11:42:38 +0300

    test unit updates

--- src/test/java/org/freelesson/springsecurityjdbc/SpringSecurityJdbcApplicationTests.java
+++ src/test/java/org/freelesson/springsecurityjdbc/SpringSecurityJdbcApplicationTests.java
@@ -1,68 +1,46 @@
 package org.freelesson.springsecurityjdbc;
 
-import static org.junit.Assert.assertEquals;
-import static  org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.freelesson.springsecurityjdbc.domain.Welcome;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import com.google.gson.Gson;
+
 
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT, classes=TestConfig.class)
+@SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class SpringSecurityJdbcApplicationTests {
-	@Autowired
-	private TestRestTemplate restTemplate;
 	
 	@Autowired
 	private MockMvc mockMvc;
 	
 	@Test
-	public void noAuth() {
-		String testName = "test";
-		String request = "/hello?name="+testName;
-		ResponseEntity<Welcome> response = restTemplate.getForEntity(request, Welcome.class);
-		assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
-	}
-	
-	@Test
-	@WithUserDetails("user@example.com")
-	public void shouldAllowUserWithNoAuthorities() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.get("/hello?name=Seb")
-				.accept(MediaType.ALL))
-		.andExpect(status().isOk())
-		.andExpect(jsonPath("$.greetings").value("Welcome Seb"));
-	}
-	@Test
-	public void shouldRejectIfNoAuthentication() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.get("/hello?name=Seb")
-				.accept(MediaType.ALL))
-		.andExpect(status().isUnauthorized());
-	
-	}
-	
-	@Test
 	public void auth() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.post("/oauth/token").header("Authorization", "Basic c3ByaW5nLXNlY3VyaXR5LWpkYmMtYXBwOnNwcmluZy1zZWN1cml0eS1qZGJjLXMjY3JAdA==").header("Content-Type", "multipart/form-data;")
-				.with(user("user@example.com").password("userPass!2#"))
+		Map<String, String> params = new HashMap<>();
+	    params.put("username", "user@example.com");
+	    params.put("password", "userPass!2#");
+	    Gson gson = new Gson();
+	    String mapVal = gson.toJson(params);
+		mockMvc.perform(MockMvcRequestBuilders.post("/oauth/token")
 				.param("grant_type", "password")
+				.content(mapVal)
+				.with(httpBasic( "spring-security-jdbc-app","spring-security-jdbc-s#cr@t"))
+				.header("Content-Type", MediaType.MULTIPART_FORM_DATA)
 				.accept(MediaType.ALL))
-		.andExpect(status().is2xxSuccessful());
-	
+		.andExpect(status().isOk());
 	}
-	
 }
